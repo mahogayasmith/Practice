422f88ba12ee1ae700dc3d99be1364e7a6bce02e
mahogayasmith <mahogayasmith4mail@gmail.com>
2024-06-24 21:29:43 +0530

    ui set1

--- src/UI.java
+++ src/UI.java
@@ -2,5 +2,6 @@ public class UI {
     public static void main(String[] args) {
         System.out.println("UI");
         System.out.println("Hello World");
+        System.out.println("Hello World1");
     }
 }
